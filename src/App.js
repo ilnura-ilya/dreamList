2d217a3adedcb3959e9a328448f3e60740c733aa
Ilnura <t.ilnura@gmail.com>
2022-01-07 12:23:43 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Dream } from './Dream';
+import image from './dream.png'
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className='container'>
+        <img className="dream" src={image} alt="dream" width="200px"/>
+      </div>
+      <div className='container'>
+      <h1>My Dream List 2022</h1>
+      </div>
+      
+      <Dream/>
+       
     </div>
   );
 }
 
 export default App;
+
